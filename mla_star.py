2a3e0509147414fade42253ec4b00391b5a8d8bd
sparsh-b <bsparshbss@gmail.com>
2022-05-06 18:04:49 +0530

    completed mla\* & tested on a few cases

--- mla_star.py
+++ mla_star.py
@@ -6,7 +6,6 @@ import heapq as hq
 def euclidean_dist(node1_x, node1_y, node2_x, node2_y):
     return sqrt((node1_x - node2_x)**2 + (node1_y - node2_y)**2)
 
-
 class Node():
     def __init__(self, x, y, l, g, task, t, parent=-1) -> None:
         self.x = x
@@ -15,7 +14,8 @@ class Node():
         self.g = g
         self.t = t # start node is at t=0
         self.task = task
-        self.h = self.compute_h()
+        self.compute_h()
+        #self.h = self.compute_h()
         self.f = self.g + self.h
         self.parent = parent
         # self.p = [self.x, self.y]
@@ -29,6 +29,9 @@ class Node():
             assert self.l == 2
             assert len(self.task) == 1
             self.h = euclidean_dist(self.x, self.y, self.task[0][0], self.task[0][1])
+    
+    def __str__(self):
+        return 'x:{} y:{} l:{} t:{} g:{} h:{} task:{}'.format(self.x, self.y, self.l, self.t, self.g, self.h, self.task)
 
 class Path():
     def __init__(self) -> None:
@@ -44,20 +47,27 @@ class Path():
     
     def add_time(self):
         self.ts = [i for i in range(self.len)]
+    
+    def __str__(self):
+        print_str = ''
+        assert len(self.xs) == len(self.ys) == len(self.ts), '{} {} {}'.format(len(self.xs), len(self.ys), len(self.ts))
+        for i in range(len(self.ys)):
+            print_str += '{} {} {} -> '.format(self.xs[i], self.ys[i], self.ts[i])
+        return print_str
 
 def collision_exists(curr_x, curr_y, curr_t, prev_x, prev_y, prev_t, paths):
     for other_ag_path in paths:
-        if curr_x == other_ag_path.xs[curr_t] and curr_y == other_ag_path.ys[curr_t]:
-            return True
-        elif curr_x == other_ag_path.xs[prev_t] and curr_y == other_ag_path.ys[prev_t] and \
-             prev_x == other_ag_path.xs[curr_t] and prev_y == other_ag_path.ys[curr_t]:
-             return True
+        if len(other_ag_path.xs) > curr_t:
+            if curr_x == other_ag_path.xs[curr_t] and curr_y == other_ag_path.ys[curr_t]:
+                return True
+            elif curr_x == other_ag_path.xs[prev_t] and curr_y == other_ag_path.ys[prev_t] and \
+                prev_x == other_ag_path.xs[curr_t] and prev_y == other_ag_path.ys[curr_t]:
+                return True
     return False
-                
 
-
-def generate_children(parent, nav_space, paths):
-    children = []
+def generate_children(parent, nav_space, paths, nodes, Q, num_generated):
+    if debug:
+        print('children:')
     for child_x in [parent.x-1, parent.x, parent.x+1]:
         for child_y in [parent.y-1, parent.y, parent.y+1]:
             if child_x == parent.x and child_y == parent.y:
@@ -77,10 +87,23 @@ def generate_children(parent, nav_space, paths):
             child_l = parent.l
             child_task = parent.task
             child_t = parent.t + 1
-            #check if already generated
-            children.append(Node(child_x, child_y, child_l, child_g, child_task, child_t, parent))
-
-    return children
+            child = Node(child_x, child_y, child_l, child_g, child_task, child_t, parent)
+            if debug:
+                print(child)
+            if (child_x, child_y, child_l) not in nodes:
+                num_generated += 1
+                hq.heappush(Q, (child.f, num_generated, child))
+                nodes[(child_x, child_y, child_l)] = child
+            else:
+                prev_cost = nodes[(child_x, child_y, child_l)].f
+                if child.f < prev_cost:
+                    nodes[(child_x, child_y, child_l)] = child
+                    for idx in range(len(Q)):
+                        if Q[idx][2].x == child_x and Q[idx][2].y == child_y and Q[idx][2].l == child_l:
+                            Q[idx] = (child.f, Q[idx][1], child)
+                            hq.heapify(Q)
+                            break
+    return Q, num_generated
 
 def backtrack(final_node):
     path = Path()
@@ -93,14 +116,19 @@ def backtrack(final_node):
 
 def mla_star(start, task, paths, nav_space): # returns a path object
     Q = []
-    hq.heappush(Q, (start.f, start))
+    nodes = {(start.x, start.y, start.l): start}
+    num_generated = 1
+    hq.heappush(Q, (start.f, num_generated, start))
     hq.heapify(Q)
     while len(Q) != 0:
-        curr_node = hq.heappop(Q)
+        _, _, curr_node = hq.heappop(Q)
+        if debug:
+            print('*******************************\nparent:')
+            print(curr_node)
         if curr_node.l == 1:
-            assert len(task) == 2
-            pick_x = task[0][0]
-            pick_y = task[0][1]
+            assert len(curr_node.task) == 2
+            pick_x = curr_node.task[0][0]
+            pick_y = curr_node.task[0][1]
             t_max = 1e5
             for other_ag_path in paths:
                 for i in range(other_ag_path.len):
@@ -110,34 +138,50 @@ def mla_star(start, task, paths, nav_space): # returns a path object
                 if t_max != 1e5:
                     break
             if curr_node.g > t_max:
+                if debug:
+                    print('T_max exceeded. Skipping expansion.')
                 continue
         if curr_node.l == 1:
-            if curr_node.x == task[0][0] and curr_node.y == task[0][1]:
-                new_node = Node(curr_node.x, curr_node.y, 2, curr_node.g, [task[1]], curr_node, curr_node.t)
-                hq.heappush(Q, (new_node.f, new_node))
+            if curr_node.x == curr_node.task[0][0] and curr_node.y == curr_node.task[0][1]:
+                new_node = Node(curr_node.x, curr_node.y, 2, curr_node.g, [curr_node.task[1]], curr_node.t, curr_node)
+                num_generated += 1
+                hq.heappush(Q, (new_node.f, num_generated, new_node))
+                nodes[(new_node.x, new_node.y, new_node.l)] = new_node
+                if debug:
+                    print('Reached pickup location. Skipping expansion.')
                 continue
         elif curr_node.l == 2:
-            assert len(task) == 1
-            if curr_node.x == task[0][0] and curr_node.y == task[0][1]:
+            assert len(curr_node.task) == 1, '{}'.format(curr_node.task)
+            if curr_node.x == curr_node.task[0][0] and curr_node.y == curr_node.task[0][1]:
                 path = backtrack(curr_node)
                 return path
         
-        children = generate_children(curr_node, nav_space, paths)
-        for child in children:
-            hq.heappush(Q, (child.f, child))
+        Q, num_generated = generate_children(curr_node, nav_space, paths, nodes, Q, num_generated)
         
     return None
 
-def get_start_locations(num_agents):
-    start_locs = [[1, 10], [1, 15], [1, 20]]
-    #for i in num_agents:
+def get_start_locations(num_agents): # outputs in (x_coordinate, y_coordinate) format
+    start_locs = [[1, 10], [1, 15], [1, 19]]
+    #start_locs = start_locs[::-1]
     return start_locs
 
-def get_tasks(num_agents):
-    tasks = [[[4,10],[0,10]], [[4,15],[0,15]], [[4,20],[0,20]]]
+def get_tasks(num_agents): # outputs in (x_coordinate, y_coordinate) format
+    tasks = [[[4,10],[0,10]], [[4,15],[0,15]], [[4,19],[0,19]]]
+    #tasks = tasks[::-1]
     return tasks
 
+def sane_starts_tasks(starts, tasks, nav_space):
+    for start in starts:
+        assert start[0] >= 0 and start[0] < nav_space.shape[1], '{} {}'.format(start[0], nav_space.shape[1])
+        assert start[1] >= 0 and start[1] < nav_space.shape[0], '{} {}'.format(start[1], nav_space.shape[0])
+    for task in tasks:
+        assert len(task) == 2
+        for i in range(2):
+            assert task[i][0] >= 0 and task[i][0] < nav_space.shape[1], '{} {}'.format(task[i][0], nav_space.shape[1])
+            assert task[i][1] >= 0 and task[i][1] < nav_space.shape[0], '{} {}'.format(task[i][1], nav_space.shape[0])
+
 if __name__ == '__main__':
+    debug = 0
     num_agents = 3
     starts = get_start_locations(num_agents)
     tasks = get_tasks(num_agents)
@@ -148,6 +192,7 @@ if __name__ == '__main__':
     # nav_space[:, 0] corresponds to left-most column (corresponding to x=0)
     # x varies from 0 to nav_space.shape[1]-1
     # y varies from 0 to nav_space.shape[0]-1
+    sane_starts_tasks(starts, tasks, nav_space)
     for i in range(num_agents):
         task = tasks[i]
         start = starts[i]
@@ -155,4 +200,8 @@ if __name__ == '__main__':
         path = mla_star(start, task, paths, nav_space)
         if path is None:
             print('No path found for agent-'+str(i))
-        paths.append(path)
+            input('Press enter to continue')
+        else:
+            paths.append(path)
+            print('Path of agent-'+str(i))
+            print(path)
